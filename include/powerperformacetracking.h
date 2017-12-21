f25598457986e7f1e12574647a8bebd6cda12b82
jkrishnavs <jkrishnavs@gmail.com>
2017-12-21 13:15:34 +0530

    stencil for tracking
    tracking code : not public

---
+++ include/powerperformacetracking.h
@@ -0,0 +1,26 @@
+#ifndef POWER_PERFORMANCE_TRACKING_H
+#define POWER_PERFORMANCE_TRACKING_H
+/******************
+ * Add tracking for power and performance here
+ * Code will change for different hardwares.
+ *
+ ******************/
+void inittracking() {
+
+
+}
+
+
+void pausetracking() {
+
+
+}
+
+
+void endtracking() {
+
+
+}
+
+
+#endif
