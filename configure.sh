b22ffd28809c0f278648312a1f7ce036cc534d79
jkrishnavs <jkrishnavs@gmail.com>
2018-01-12 12:00:50 +0530

    minor configure edits

--- configure.sh
+++ configure.sh
@@ -9,10 +9,11 @@ if [ -z "$(gcc --version | head -n1 | cut -d" " -f4)"  ];
   then
 	echo "gcc version less than 6, Task loop might not work"
         echo "CCINST = gcc" > Makefile.in
+	echo "TASKLOOP_DEFINED= No" > Makefile.in
   else
       echo "GCC version is ","$(gcc --version | head -n1 | cut -d" " -f4)"
           echo "CCINST = gcc" > Makefile.in
-      echo "TASKLOOP_DEFINED = YES" > Makefile.in
+      echo "TASKLOOP_DEFINED= yes" > Makefile.in
 fi
 
 details=$(uname -a)
