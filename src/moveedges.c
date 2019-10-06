05f907ce554da563e384ac8f3e68fd48ebdd6838
jkrishnavs <jkrishnavs@gmail.com>
2019-10-06 19:49:22 +0530

    fixing runtime errors in move edges program

--- src/moveedges.c
+++ src/moveedges.c
@@ -37,15 +37,20 @@ graph* moveedges(graph *G) {
   for(node_t i =0; i< G->numNodes; i++) {
      nodeT[i]  = 0; //G->begin[i+1] - G->begin[i];
   }
-
+  node_t start, level;
   for(node_t i = 0; i< max;i++) {
-    node_t start = rand() % max; // start point to replace
-    node_t level = G->begin[start+1] - G->begin[start];
-    node_t end = rand() % level; // end point of edge
+    
+    do{
+      start = rand() % max; // start point to replace
+      level = G->begin[start+1] - G->begin[start];
+    } while(level ==0);
 
+    node_t end = rand() % level; // end point of edge
+    
     if(flags[G->begin[start] + end ] == 0) {
       // new start and end points.
       node_t new_start = rand()% max; 
+      //printf("old %d, edge = %d, new = %d \n", start, end, new_start);
       flags[G->begin[start] + end] = 1;
       nodeT[new_start]++;
     } else {
@@ -68,33 +73,64 @@ graph* moveedges(graph *G) {
       new_end_points = (node_t*) realloc (new_end_points,sizeof(node_t) * sub_array_size);
     }
     nep_index = 0;
+
     // find new end points
     for(edge_t e = 0; e< nodeT[i]; e++) {
-      new_end_points[i] = rand()%max;
+      node_t new_end = rand()%max;
+      int flag_e = 0;
+      for(edge_t e1 = 0;e1 < e; e1++) {
+	if(new_end_points[e1] == new_end) {
+	  // edge already generated
+	  flag_e = 1;
+	  break;
+	}
+      }
+      for(node_t old_e = G->begin[i]; old_e < G->begin[i+1]; old_e++) {
+	if(G->node_idx[old_e] == new_end && flags[old_e] == 0) {
+	  // the edge is already present in original graph
+	  flag_e = 1;
+	}
+      }
+      if(new_end == i || flag_e == 1) {
+	// self loop , avoid.
+	e--;
+	continue;
+      }
+      if(e>=0)
+	new_end_points[e] = new_end;
     }
+    // sort the end points
     if(nodeT[i] >1)
       qsort(new_end_points, nodeT[i], sizeof(node_t), comp);
+    
+    // update the array indices
     if(i>0) {
       G->begin[i-1] = prev_i;
       prev_i = ep_index;
     }  
+
+    // place the new nodes in appropriate places.
     for(edge_t ev = G->begin[i]; ev < G->begin[i+1]; ev++) {
       if(!flags[ev]) {
 	while(new_end_points[nep_index] < G->node_idx[ev] && nep_index < nodeT[i])
 	  end_points[ep_index++] = new_end_points[nep_index++];
 	end_points[ep_index++] = G->node_idx[ev];
       }
-    } // copy the rest of new_end_points;
+    } 
     
-    
+    // copy the rest of new_end_points;
     while(nep_index < nodeT[i]) {
       end_points[ep_index++] = new_end_points[nep_index++];
     }
     
   }
+
+  G->begin[G->numNodes-1] = prev_i;
+
+
   assert(ep_index == G->numEdges);
   node_t* old_edges = G->node_idx;
-  G->node_idx  =  new_end_points;
+  G->node_idx  =  end_points;
   free(old_edges);
   free(new_end_points);
   free(flags);
@@ -135,23 +171,23 @@ int runalgo(int argc,char** argv) {
   }
   graph* newG = moveedges(G);
 
-  avgincident = ((double) G->numEdges )/ G->numNodes;
+  avgincident = ((double) newG->numEdges )/ newG->numNodes;
   
-  writeBackGraph(newG, argv[4]);
-  clusterCoeff = avgClusterCoeff(G);
-  aed = avgEdgeDistance(G);
-  dim = diameter(G);
-  sparsityMeasure  = sparsity(G);
-  double T = triangle_counting(G);
+  writeEdgeListGraph(newG, argv[3]);
+  clusterCoeff = avgClusterCoeff(newG);
+  aed = avgEdgeDistance(newG);
+  dim = diameter(newG);
+  sparsityMeasure  = sparsity(newG);
+  double T = triangle_counting(newG);
 
   avgClusterCoeff(newG);
   avgEdgeDistance(newG);
   diameter(newG);
   sparsity(newG);
   triangle_counting(newG);
-  scaledT = ((double) T )/ G->numNodes; 
+  scaledT = ((double) T )/ newG->numNodes; 
   sccIndex(newG); 
-  writeSchema(argv[3]);
+  writeSchema(argv[4]);
   return 0;
 }
 
